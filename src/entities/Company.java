54e5fa27b940e1d96ceaaa59a2442dfe8117a92d
Axl Andrade <axlsandrade@outlook.com>
2022-03-03 18:30:49 -0300

    Added Company class attributes, constructors, getters, setters and methods

--- src/entities/Company.java
+++ src/entities/Company.java
@@ -2,10 +2,32 @@ package entities;
 
 public class Company extends TaxPayer {
 
+	private int numberOfEmployees;
+	
+	public Company() {
+		super();
+	}
+	
+	public Company(String name, Double anualIncome, int numberOfEmployees) {
+		super(name, anualIncome);
+		this.numberOfEmployees = numberOfEmployees;
+	}
+
+	public int getNumberOfEmployees() {
+		return numberOfEmployees;
+	}
+
+	public void setNumberOfEmployees(int numberOfEmployees) {
+		this.numberOfEmployees = numberOfEmployees;
+	}
+
 	@Override
 	public double tax() {
-		// TODO Auto-generated method stub
-		return 0;
+		if (numberOfEmployees > 10) {
+			return (anualIncome * 14.0/100);
+		} else {
+			return (anualIncome * 16.0/100);
+		}
 	}
 
 }
